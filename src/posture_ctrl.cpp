54261bb4094c6bcce643f2c7008a38111bb3b5ae
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-06-21 14:11:53 +0900

    added srv

--- src/posture_ctrl.cpp
+++ src/posture_ctrl.cpp
@@ -32,7 +32,8 @@
 
    bool POSTURE::actions_srv(unitree_a1::actions::Request& req, unitree_a1::actions::Response& res){
       geometry_msgs::Twist cmd;
-      
+      std_msgs::Bool exe;
+      exe.data = true;
       if(req.action ==0){
          //pass through
       }else if (req.action == 1){
@@ -45,12 +46,13 @@
 
       clock_gettime(CLOCK_MONOTONIC, &start); fstart=(double)start.tv_sec + ((double)start.tv_nsec/1000000000.0);
       clock_gettime(CLOCK_MONOTONIC, &stop); fstop=(double)stop.tv_sec + ((double)stop.tv_nsec/1000000000.0);
-      action_execution.publish(true);
+      action_execution.publish(exe);
       while(fstop < req.duration){
          action_cmd.publish(cmd);
          clock_gettime(CLOCK_MONOTONIC, &stop); fstop=(double)stop.tv_sec + ((double)stop.tv_nsec/1000000000.0);
       }
-      action_execution.publish(false);
+      exe.data = false;
+      action_execution.publish(exe);
       
    }
 
